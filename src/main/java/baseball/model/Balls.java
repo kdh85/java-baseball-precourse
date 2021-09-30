cfbe21f4ace5fe31f50d8a3c389e4d8c087d833c
kdh85 <modudamba@naver.com>
2021-09-30 17:36:31 +0900

    feat: 야구게임 볼들의 모음인 볼즈 객체 생성 * 숫자들의 콜랙션을 주입하여 볼을 3개 생성함. * 볼이 3개가 생성안될 경우 에러 반환. * 볼을 비교하여 볼타입들을 반환하는 메소드 추가.

---
+++ src/main/java/baseball/model/Balls.java
@@ -0,0 +1,91 @@
+package baseball.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Balls {
+
+	private static final String MSG_ERROR_BALLS_SIZE = "공의 수가 부족합니다.";
+	private static final int LIMIT_BALLS_SIZE = 3;
+
+	private final List<Ball> balls;
+
+	public Balls(List<Integer> numbers) {
+		this.balls = Collections.unmodifiableList(createBalls(numbers));
+	}
+
+	private List<Ball> createBalls(List<Integer> numbers) {
+		AtomicInteger atomicInteger = new AtomicInteger();
+
+		List<Ball> newBalls = new ArrayList<>();
+		for (Integer number : numbers) {
+			newBalls.add(new Ball(number, atomicInteger.getAndIncrement()));
+		}
+
+		validationBalls(newBalls);
+
+		return newBalls;
+	}
+
+	private void validationBalls(List<Ball> balls) {
+		if (balls.size() != LIMIT_BALLS_SIZE) {
+			throw new IllegalArgumentException(MSG_ERROR_BALLS_SIZE);
+		}
+	}
+
+	public BallType matchOneBall(Ball targetBall) {
+
+		BallType resultBallType = BallType.NOTHING;
+
+		for (Ball ball : balls) {
+			resultBallType = strikeOrBall(targetBall, resultBallType, ball);
+		}
+		return resultBallType;
+	}
+
+	private BallType strikeOrBall(Ball targetBall, BallType resultBallType, Ball ball) {
+		if(isStrikeOrBall(targetBall, ball)){
+			resultBallType = ball.pitchResult(targetBall);
+		}
+		return resultBallType;
+	}
+
+	private boolean isStrikeOrBall(Ball targetBall, Ball ball) {
+		return !ball.pitchResult(targetBall).equals(BallType.NOTHING);
+	}
+
+	public List<BallType> matchThreeBalls(Balls targetBalls) {
+		List<BallType> results = new ArrayList<>();
+
+		for (Ball targetBall : targetBalls.balls) {
+			results.add(this.matchOneBall(targetBall));
+		}
+
+		return results;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Balls balls1 = (Balls)o;
+		return Objects.equals(balls, balls1.balls);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(balls);
+	}
+
+	@Override
+	public String toString() {
+		return "Balls{" +
+			"balls=" + balls +
+			'}';
+	}
+}
