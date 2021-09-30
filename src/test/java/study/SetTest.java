0f284fa456fc92c4f8bf9dcb49dfe97b6fae9440
kdh85 <modudamba@naver.com>
2021-09-30 11:58:14 +0900

    test : 학습테스트 Set 콜랙션 contain 테스트코드 추가 * 미리 생선된 콜랙션에 포함된 값을 검증하기 위해 contain 메소드를 사용하여 테스트코드를 작성. * 중복되는 과정을 제거하기 위해 ParameterizedTest 테스트로 테스트코드를 리팩토링하고 ValueSource를 사용하여 가변적인 값을 주입하도록 리팩토링.

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -8,6 +8,8 @@ import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
 
@@ -26,5 +28,12 @@ public class SetTest {
 	@Test
 	void setSizeTest() {
 		assertThat(numbers.size()).isEqualTo(3);
+	}
+
+	@DisplayName("Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인한다.")
+	@ParameterizedTest
+	@ValueSource(ints = {1,2,3})
+	void contains(int number) {
+		assertThat(numbers.contains(number)).isTrue();
 	}
 }
