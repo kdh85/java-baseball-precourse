370199cf82c5c1ed23b42cbe1707cae15862e584
kdh85 <modudamba@naver.com>
2021-09-30 14:10:59 +0900

    test: 야구게임 볼넘버 객체 테스트코드 리팩토링 * 숫자를 입력받아 볼넘버 객체를 생성하는 테스트코드에 실패 케이스를 추가하도록 리팩토링.

--- src/test/java/baseball/model/BallNumberTest.java
+++ src/test/java/baseball/model/BallNumberTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -19,9 +18,10 @@ class BallNumberTest {
 	}
 
 	@DisplayName("숫자를 입력하여 각각 동일한 볼넘버 객체를 생성한다.")
-	@Test
-	void createBallNumberTest() {
-		assertThat(ballNumber).isEqualTo(new BallNumber(1));
+	@ParameterizedTest
+	@CsvSource(value = {"1|true", "2|false"}, delimiter = '|')
+	void createBallNumberTest(int number, boolean isSame) {
+		assertThat(ballNumber.equals(new BallNumber(number))).isEqualTo(isSame);
 	}
 
 	@DisplayName("1~9를 벗어나는 숫자를 입력하여 볼넘버를 생성시 에러가 발생한다.")
