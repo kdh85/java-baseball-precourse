f8b51e61f3a7993ba82184b405050bcdc8205ea8
kdh85 <modudamba@naver.com>
2021-09-30 14:40:33 +0900

    test: 야구게임 볼속성을 비교하여 볼타입을 반환하는 메소드 추가에 따른 테스트코드 작성 * 볼넘버와 볼위치 값을 기준으로 둘다 같을 경우 볼타입 스트라이크를 반환하는 테스트코드 작성. * 볼넘버만 같으면 볼타입 볼을 반환하는 테스트코드 작성. * 볼넘버, 볼위치 값이 전부 틀린 경우 볼 타입 낫싱을 반환하는 테스트코드 작성.

--- src/test/java/baseball/model/BallTest.java
+++ src/test/java/baseball/model/BallTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -21,5 +22,22 @@ class BallTest {
 	@CsvSource(value = {"1|0|true", "2|0|false", "1|1|false"}, delimiter = '|')
 	void createBallTest(int number, int otherNumber, boolean isSame) {
 		assertThat(ball.equals(new Ball(number, otherNumber))).isEqualTo(isSame);
+	}
+	@DisplayName("볼을 비교하여 같으면 스트라이크를 반환한다.")
+	@Test
+	void pitchResult_nothing_Test() {
+		assertThat(ball.pitchResult(new Ball(2, 0))).isEqualTo(BallType.NOTHING);
+	}
+
+	@DisplayName("볼을 비교하여 같으면 스트라이크를 반환한다.")
+	@Test
+	void pitchResult_ball_Test() {
+		assertThat(ball.pitchResult(new Ball(1, 1))).isEqualTo(BallType.BALL);
+	}
+
+	@DisplayName("볼을 비교하여 같으면 스트라이크를 반환한다.")
+	@Test
+	void pitchResult_strike_Test() {
+		assertThat(ball.pitchResult(new Ball(1, 0))).isEqualTo(BallType.STRIKE);
 	}
 }
