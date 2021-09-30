0ad31f3d570ffd8a8ce49c481614d31901f8d648
kdh85 <modudamba@naver.com>
2021-09-30 14:07:53 +0900

    test: 야구게임 볼위치 객체 테스트코드 생성 * 숫자를 입력받아 볼위치 객체를 생성하는 테스트코드 생성. * 입력값이 0~2를 벗어나는 경우 에러를 리턴을 검증하는 테스트코드 생성.

---
+++ src/test/java/baseball/model/BallPositionTest.java
@@ -0,0 +1,35 @@
+package baseball.model;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class BallPositionTest {
+
+	private BallPosition ballPosition;
+
+	@BeforeEach
+	void setUp() {
+		ballPosition = new BallPosition(0);
+	}
+
+	@DisplayName("볼위치값을 주입하여 동일한 볼위치 객체를 생성한다.")
+	@ParameterizedTest
+	@CsvSource(value = {"0|true", "1|false"}, delimiter = '|')
+	void createBallPositionTest(int position, boolean isSame) {
+		assertThat(ballPosition.equals(new BallPosition(position))).isEqualTo(isSame);
+	}
+
+	@DisplayName("0~2를 벗어나는 위치값을 주입받으면 생성시 에러가 발생한다.")
+	@ParameterizedTest
+	@ValueSource(ints = {-1, 3})
+	void validationPositionTest(int number) {
+		assertThatThrownBy(
+			() -> new BallPosition(number)
+		).isInstanceOf(IllegalArgumentException.class);
+	}
+}
