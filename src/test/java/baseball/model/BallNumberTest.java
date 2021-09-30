3df84a5f4f0c1731a4c09f19d612a8aa32e5f73f
kdh85 <modudamba@naver.com>
2021-09-30 13:57:44 +0900

    test: 야구게임 볼넘버 객체 테스트코드 생성 * 숫자를 입력받아 볼넘버 객체를 생성하는 테스트코드 작성. * 입력값이 1~9를 벗어나는 경우 에러를 리턴을 검증하는 테스트코드 작성. * 볼객체간에 비교를 통하여 동일한지 여부를 판단하는 메소드 검증 테스트코드 작성.

---
+++ src/test/java/baseball/model/BallNumberTest.java
@@ -0,0 +1,42 @@
+package baseball.model;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class BallNumberTest {
+
+	private BallNumber ballNumber;
+
+	@BeforeEach
+	void setUp() {
+		ballNumber = new BallNumber(1);
+	}
+
+	@DisplayName("숫자를 입력하여 각각 동일한 볼넘버 객체를 생성한다.")
+	@Test
+	void createBallNumberTest() {
+		assertThat(ballNumber).isEqualTo(new BallNumber(1));
+	}
+
+	@DisplayName("1~9를 벗어나는 숫자를 입력하여 볼넘버를 생성시 에러가 발생한다.")
+	@ParameterizedTest
+	@ValueSource(ints = {0, 10})
+	void validationBallNumberTest(int number) {
+		assertThatThrownBy(
+			() -> new BallNumber(number)
+		).isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@DisplayName("볼넘버 객체간에 비교를 통해 동일여부를 반환한다.")
+	@ParameterizedTest
+	@CsvSource(value = {"1|true", "2|false"}, delimiter = '|')
+	void matchBallNumberTest(int targetNumber, boolean isSame) {
+		assertThat(ballNumber.isSameBallNumber(new BallNumber(targetNumber))).isEqualTo(isSame);
+	}
+}
