592feb82ff353d13e9509a9c163290ca76dc57e4
kdh85 <modudamba@naver.com>
2021-09-30 12:03:28 +0900

    test : 학습테스트 Set 콜랙션 contain 테스트코드 리팩토링 * 기존의 contains 메소드를 실패하는 경우를 추가하기 위해 결과값을 같이 할당하고 주입되는 값 결과가 일치하는지 확인하는 코드로  리팩토링을 수행.

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
@@ -32,8 +33,8 @@ public class SetTest {
 
 	@DisplayName("Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인한다.")
 	@ParameterizedTest
-	@ValueSource(ints = {1,2,3})
-	void contains(int number) {
-		assertThat(numbers.contains(number)).isTrue();
+	@CsvSource(value = {"1|true","2|true","3|true","4|false","5|false"}, delimiter = '|')
+	void contains(int number, boolean result) {
+		assertThat(numbers.contains(number)).isEqualTo(result);
 	}
 }
