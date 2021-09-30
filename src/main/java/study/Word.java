6c8d739c86dd0e0938d1b64e3766a2c501a00046
kdh85 <modudamba@naver.com>
2021-09-30 11:33:23 +0900

    feat : 학습테스트 String 클래스 substring 예제 구현코드 생성 * 문자열을 입력받아서 객체를 생성한 뒤, 특정 값을 가공하기 위해 자릿수를 선정하여 substring 한 결과를 반환하는 메소드 구현.

--- src/main/java/study/Word.java
+++ src/main/java/study/Word.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 public class Word {
 
+	private static final int START_INDEX = 1;
+	private static final int END_INDEX = 4;
+
 	private final String word;
 
 	public Word(String word) {
@@ -13,5 +16,9 @@ public class Word {
 
 	public List<String> splitWord(final String splitMark){
 		return Arrays.asList(word.split(splitMark));
+	}
+
+	public String substringWord() {
+		return word.substring(START_INDEX, END_INDEX);
 	}
 }
