cf9a1f44175186e4a20f50cad2bf99a901f21e12
kdh85 <modudamba@naver.com>
2021-10-01 14:14:46 +0900

    test: 야구게임 스트라이크 카운트 객체 테스트코드 생성 * 볼들의 판정을 기준으로 스트라이크인 경우 스트라이크 공의 갯수만큼 스트라이크 카운트값을 증가하여 돌려주는 객체를 생성 테스트코 작성. * 스트라이크 판정 한개당 스트라이크 카운트를 1씩 증가시키는 메소드 테스트코드 작성. * 스트라이크3개인 경우 게임종료의 기준이 되므로 이를 확인할 판별값 메소드 테스트코드 작성.

---
+++ src/test/java/baseball/model/StrikeCountTest.java
@@ -0,0 +1,42 @@
+package baseball.model;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class StrikeCountTest {
+
+	private StrikeCount strikeCount;
+
+	@BeforeEach
+	void setUp() {
+		strikeCount = new StrikeCount(0);
+	}
+
+	@DisplayName("스트라이크 카운트 객체를 생성한다.")
+	@Test
+	void createStrikeTest() {
+		assertThat(strikeCount).isEqualTo(new StrikeCount(0));
+	}
+
+	@DisplayName("스트라이크 카운트값을 1증가하는지 확인한다.")
+	@Test
+	void increaseStrikeCountTest() {
+		strikeCount.increaseCount();
+		assertThat(strikeCount).isEqualTo(new StrikeCount(1));
+	}
+
+	@DisplayName("3스크라이크로 아웃인지를 확인한다.")
+	@ParameterizedTest
+	@CsvSource(value = {"3|true", "2|false"}, delimiter = '|')
+	void threeStrikeOUtTest(int count, boolean isOut) {
+		for (int index = 0; index < count; index++) {
+			strikeCount.increaseCount();
+		}
+		assertThat(strikeCount.isThreeStrikeOut()).isEqualTo(isOut);
+	}
+}
