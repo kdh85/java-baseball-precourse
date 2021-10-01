cf74bbe02295cfbfb1037063ee2d07c03b690989
kdh85 <modudamba@naver.com>
2021-10-01 14:18:53 +0900

    feat: 야구게임 3구 피칭결과 객체 생성 * 공3개의 판별결과 값을 기준으로 스트라이크, 볼, 낫싱의 카운트를 각각의 볼타입별로 카운트하여 반환하는 객체 생성.

---
+++ src/main/java/baseball/model/GameResult.java
@@ -0,0 +1,103 @@
+package baseball.model;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class GameResult {
+
+	private static final int MAX_BALL_COUNT = 3;
+	private static final int DEFAULT_COUNT = 0;
+
+	private final StrikeCount strikeCount;
+
+	private final BallCount ballCount;
+
+	public GameResult() {
+		this.strikeCount = new StrikeCount(DEFAULT_COUNT);
+		this.ballCount = new BallCount(DEFAULT_COUNT);
+	}
+
+	public void calculateScore(final List<BallType> ballTypes) {
+
+		for (BallType ballType : ballTypes) {
+			increaseStrike(ballType);
+			increaseBall(ballType);
+		}
+	}
+
+	private void increaseBall(BallType ballType) {
+		if (ballType.isBall()) {
+			ballCount.increaseCount();
+		}
+	}
+
+	private void increaseStrike(BallType ballType) {
+		if (ballType.isStrike()) {
+			strikeCount.increaseCount();
+		}
+	}
+
+	public Map<BallType, Integer> pitchResult() {
+
+		Map<BallType, Integer> result = new HashMap<>();
+
+		for (BallType value : BallType.values()) {
+			addStrike(result, value);
+			addBall(result, value);
+			addNothing(result, value);
+		}
+		return result;
+	}
+
+	private void addNothing(Map<BallType, Integer> result, BallType value) {
+		if (value.isNothing()) {
+			result.put(value, calculateNothing());
+		}
+	}
+
+	private void addBall(Map<BallType, Integer> result, BallType value) {
+		if (value.isBall()) {
+			result.put(value, ballCount.count());
+		}
+	}
+
+	private void addStrike(Map<BallType, Integer> result, BallType value) {
+		if (value.isStrike()) {
+			result.put(value, strikeCount.count());
+		}
+	}
+
+	private int calculateNothing() {
+		return MAX_BALL_COUNT - strikeCount.count() - ballCount.count();
+	}
+
+	public boolean isGameEnd() {
+		return strikeCount.isThreeStrikeOut();
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		GameResult that = (GameResult)o;
+		return strikeCount == that.strikeCount && ballCount == that.ballCount;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(strikeCount, ballCount);
+	}
+
+	@Override
+	public String toString() {
+		return "GameResult{" +
+			"strike=" + strikeCount +
+			", ball=" + ballCount +
+			'}';
+	}
+
+}
