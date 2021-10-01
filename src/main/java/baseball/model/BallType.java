cc219dd2f48bc75309d186239f3447a0aea8b382
kdh85 <modudamba@naver.com>
2021-10-01 14:09:03 +0900

    refactor: 야구게임 볼타입 타입별 판별 추가 * 스트라이크, 볼, 낫싱인지를 판별하는 메소드 추가.

--- src/main/java/baseball/model/BallType.java
+++ src/main/java/baseball/model/BallType.java
@@ -1,5 +1,17 @@
 package baseball.model;
 
 public enum BallType {
-	NOTHING, BALL, STRIKE
+	NOTHING, BALL, STRIKE;
+
+	public boolean isStrike() {
+		return this.equals(STRIKE);
+	}
+
+	public boolean isBall() {
+		return this.equals(BALL);
+	}
+
+	public boolean isNothing(){
+		return this.equals(NOTHING);
+	}
 }
