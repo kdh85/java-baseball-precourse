77ab7f7bfb9d121abec77076b8c91eb0d2cf795c
kdh85 <modudamba@naver.com>
2021-10-01 14:15:31 +0900

    feat: 야구게임 볼 카운트 객체 생성 * 볼들의 판정을 기준으로 볼인 경우 볼인 공의 갯수만큼 볼 카운트값을 증가하여 돌려주는 객체를 생성. * 볼 판정 한개당 볼 카운트를 1씩 증가시키는 메소드 작성.

---
+++ src/main/java/baseball/model/BallCount.java
@@ -0,0 +1,44 @@
+package baseball.model;
+
+import java.util.Objects;
+
+public class BallCount {
+
+	private static final int INCREASE_COUNT = 1;
+
+	private int ballCount;
+
+	public BallCount(int ballCount) {
+		this.ballCount = ballCount;
+	}
+
+	public void increaseCount(){
+		ballCount += INCREASE_COUNT;
+	}
+
+	public Integer count(){
+		return ballCount;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		BallCount ballCount1 = (BallCount)o;
+		return ballCount == ballCount1.ballCount;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(ballCount);
+	}
+
+	@Override
+	public String toString() {
+		return "BallCount{" +
+			"ballCount=" + ballCount +
+			'}';
+	}
+}
