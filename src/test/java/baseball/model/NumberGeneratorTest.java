7eaa4a0f7a38d0b45aeb211aab5f68c8b70b77f5
kdh85 <modudamba@naver.com>
2021-10-01 17:42:09 +0900

    test: 야구게임 번호입력 유틸객체 테스트코드 생성 * 자동으로 3개의 랜덤한 번호 콜랙션을 생성하는 메소드 테스트코드 작성. * 입력하는 문자열을 통해 3개의 번호 콜랙션을 생성하는 메소드 테스트코드 작성. * 입력하는 값에 대한 값 검증 테스트코드 작성.

---
+++ src/test/java/baseball/model/NumberGeneratorTest.java
@@ -0,0 +1,33 @@
+package baseball.model;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class NumberGeneratorTest {
+
+	@DisplayName("랜덤으로 생성한 중복없는 숫자 3개를 반환한다.")
+	@Test
+	void autoNumberCreateTest() {
+		List<Integer> autoValues = NumberGenerator.autoNumberGenerator();
+		assertThat(autoValues.size()).isEqualTo(3);
+	}
+
+	@DisplayName("직접입려한 3자리수를 각각 나눠서 숫자 3개를 반환한다.")
+	@Test
+	void manualNumberCreateTest() {
+		List<Integer> manualValues = NumberGenerator.manualNumberGenerator("123");
+		assertThat(manualValues.size()).isEqualTo(3);
+	}
+
+	@DisplayName("직접입력시 중복되는 번호가 들어가면 에러가 발생한다.")
+	@Test
+	void duplicateTest() {
+		assertThatThrownBy(
+			() -> NumberGenerator.manualNumberGenerator("113")
+		).isInstanceOf(IllegalArgumentException.class);
+	}
+}
