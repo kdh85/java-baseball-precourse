aa87ca3591b8921a5634c043a0b513391b7c094c
kdh85 <modudamba@naver.com>
2021-10-01 14:14:21 +0900

    feat: 야구게임 스트라이크 카운트 객체 생성 * 볼들의 판정을 기준으로 스트라이크인 경우 스트라이크 공의 갯수만큼 스트라이크 카운트값을 증가하여 돌려주는 객체를 생성. * 스트라이크 판정 한개당 스트라이크 카운트를 1씩 증가시키는 메소드 생성. * 스트라이크3개인 경우 게임종료의 기준이 되므로 이를 확인할 판별값 메소드를 생성.

---
+++ src/main/java/baseball/model/StrikeCount.java
@@ -0,0 +1,50 @@
+package baseball.model;
+
+import java.util.Objects;
+
+public class StrikeCount {
+
+	private static final int INCREASE_COUNT = 1;
+	private static final int OUT_COUNT = 3;
+
+	private int strikeCount;
+
+	public StrikeCount(int strikeCount) {
+		this.strikeCount = strikeCount;
+	}
+
+	public void increaseCount() {
+		this.strikeCount += INCREASE_COUNT;
+	}
+
+	public Integer count() {
+		return strikeCount;
+	}
+
+	public boolean isThreeStrikeOut(){
+		return strikeCount == OUT_COUNT;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		StrikeCount that = (StrikeCount)o;
+		return strikeCount == that.strikeCount;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(strikeCount);
+	}
+
+	@Override
+	public String toString() {
+		return "StrikeCount{" +
+			"strikeCount=" + strikeCount +
+			'}';
+	}
+
+}
