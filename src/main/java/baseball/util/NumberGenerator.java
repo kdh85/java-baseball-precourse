937a6e67d611dd9beb2de8e26cc9fe2f92eadb19
kdh85 <modudamba@naver.com>
2021-10-02 10:10:05 +0900

    refactor: 야구게임 볼생성 유틸 페키지 경로 변경 및 메소드 반환 콜랙션 변경 * 해당 클래스를 util 페키지로 이동. * 생성되는 번호의 순서값 보장을 위해 정렬순서를 보장하는 LinkedList 콜랙션으로 변경 리팩토링.

--- src/main/java/baseball/util/NumberGenerator.java
+++ src/main/java/baseball/util/NumberGenerator.java
@@ -1,7 +1,7 @@
-package baseball.model;
+package baseball.util;
 
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
@@ -9,17 +9,16 @@ import nextstep.utils.Randoms;
 
 public class NumberGenerator {
 
-	private static final int START_NUMBER_SIZE = 0;
-	private static final int END_NUMBER_SIZE = 2;
 	private static final int MIN_NUMBER = 1;
 	private static final int MAX_NUMBER = 9;
 	private static final int LIMIT_NUMBER_SIZE = 3;
 	private static final String SPLIT_CONDITION = "";
 	private static final String MSG_ERROR_NOT_ENOUGH_NUMBER = "입력값이 충분치 않습니다. 중복없는 3개의 수를입력해주세요.";
 
-	public static List<Integer> autoNumberGenerator() {
-		List<Integer> numbers = new ArrayList<>();
-		for (int i = START_NUMBER_SIZE; i <= END_NUMBER_SIZE; i++) {
+	public static LinkedList<Integer> autoNumberGenerator() {
+
+		LinkedList<Integer> numbers = new LinkedList<>();
+		while (numbers.size() != LIMIT_NUMBER_SIZE) {
 			addNumber(numbers, Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER));
 		}
 		return numbers;
@@ -31,12 +30,12 @@ public class NumberGenerator {
 		}
 	}
 
-	public static List<Integer> manualNumberGenerator(String numberValues) {
-		return new ArrayList<>(splitNumbers(numberValues));
+	public static LinkedList<Integer> manualNumberGenerator(String numberValues) {
+		return new LinkedList<>(splitNumbers(numberValues));
 	}
 
-	private static Set<Integer> splitNumbers(String numberValues) {
-		Set<Integer> splitNumbers = new HashSet<>();
+	private static LinkedHashSet<Integer> splitNumbers(String numberValues) {
+		LinkedHashSet<Integer> splitNumbers = new LinkedHashSet<>();
 
 		for (String numberValue : numberValues.split(SPLIT_CONDITION)) {
 			splitNumbers.add(Integer.valueOf(numberValue));
@@ -48,7 +47,7 @@ public class NumberGenerator {
 	}
 
 	private static void validationManualNumbers(Set<Integer> numberSet) {
-		if(numberSet.size() != LIMIT_NUMBER_SIZE){
+		if (numberSet.size() != LIMIT_NUMBER_SIZE) {
 			throw new IllegalArgumentException(MSG_ERROR_NOT_ENOUGH_NUMBER);
 		}
 	}
