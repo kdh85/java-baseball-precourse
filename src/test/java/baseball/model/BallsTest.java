6f31ef3d8ec9b866df6107603f62b00d801f6572
kdh85 <modudamba@naver.com>
2021-10-02 10:04:11 +0900

    test: 야구게임 볼즈 객체 생성자 주입 파라메터 콜랙션 변경으로 인한 테스트코드 리팩토링 * 생성되는 번호의 순서값 보장을 위해 정렬순서를 보장하는 LinkedList 콜랙션으로 변경으로 인한 테스트코드 리팩토링.

--- src/test/java/baseball/model/BallsTest.java
+++ src/test/java/baseball/model/BallsTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -16,23 +17,23 @@ class BallsTest {
 
 	@BeforeEach
 	void setUp() {
-		balls = new Balls(Arrays.asList(1, 2, 3));
+		balls = new Balls(new LinkedList<>(Arrays.asList(1, 2, 3)));
 	}
 
 	@DisplayName("볼들을 넣어서 볼즈를 생성한다.")
 	@Test
 	void createBallsTest() {
-		assertThat(balls).isEqualTo(new Balls(Arrays.asList(1, 2, 3)));
+		assertThat(balls).isEqualTo(new Balls(new LinkedList<>(Arrays.asList(1, 2, 3))));
 	}
 
 	@DisplayName("공의수가 3개가 충족되지 않으면 에러를 반환한다.")
 	@Test
 	void validationBallsTest() {
 		assertAll(
-			() -> assertThatThrownBy(() -> new Balls(Arrays.asList(1, 2)))
+			() -> assertThatThrownBy(() -> new Balls(new LinkedList<>(Arrays.asList(1, 2))))
 				.isInstanceOf(IllegalArgumentException.class),
 
-			() -> assertThatThrownBy(() -> new Balls(Arrays.asList(1, 2, 3, 4)))
+			() -> assertThatThrownBy(() -> new Balls(new LinkedList<>(Arrays.asList(1, 2, 3, 4))))
 				.isInstanceOf(IllegalArgumentException.class)
 		);
 	}
@@ -64,7 +65,7 @@ class BallsTest {
 	@DisplayName("생성된 공들을 상대로 공3개를 비교해서 전체 볼 판정을 반환받는다.")
 	@Test
 	void matchThreeBalls() {
-		List<BallType> ballTypes = balls.matchThreeBalls(new Balls(Arrays.asList(1, 3, 4)));
+		List<BallType> ballTypes = balls.matchThreeBalls(new Balls(new LinkedList<>(Arrays.asList(1, 3, 4))));
 
 		assertThat(ballTypes).containsExactly(BallType.STRIKE, BallType.BALL, BallType.NOTHING);
 	}
