a989465d8bdec30ebe3749f1b8626d32c787ec0c
kdh85 <modudamba@naver.com>
2021-10-02 16:32:06 +0900

    feat: 야구게임 게임 객체 테스트코드 생성 * 3스트라이크 기준 경기 한판 종료 메소드 테스트코드 작성. * 입력값을 받아 재경기 여부 결정 메소드 테스트코드 작성.

---
+++ src/test/java/baseball/model/GameTest.java
@@ -0,0 +1,26 @@
+package baseball.model;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class GameTest {
+
+	@DisplayName("입력값 1,2를 받아서 재시작 여부를 반환한다.")
+	@ParameterizedTest
+	@CsvSource(value = {"1|true", "2|false"}, delimiter = '|')
+	void isContinueGameTest(int mode, boolean isRestart) {
+		Game game = new Game();
+		assertThat(game.isContinueGame(mode)).isEqualTo(isRestart);
+	}
+
+	@DisplayName("경기 종료 여부를 반환한다..")
+	@Test
+	void clearGameTest() {
+		Game game = new Game();
+		assertThat(game.isGameEnd()).isEqualTo(false);
+	}
+}
