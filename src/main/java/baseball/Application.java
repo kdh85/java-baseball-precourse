7ed1a05a7c38b35cc0de3d1ee8854bf26370acff
kdh85 <modudamba@naver.com>
2021-10-02 11:40:29 +0900

    feat: 야구게임 실행 메인 생성 * 야구게임 실행을 위한 메인 구현.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,57 @@
 package baseball;
 
+import baseball.model.Balls;
+import baseball.model.GameResult;
+import baseball.util.NumberGenerator;
+import baseball.view.ResultView;
+import baseball.view.RunningView;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO 숫자 야구 게임 구현
-    }
+
+	private static final int RESTART_MODE = 1;
+	private static final int LIMIT_INSERT_NUMBER_SIZE = 3;
+	private static final String NUMBER_PATTERN = "^[0-9]*$";
+
+	public static void main(String[] args) {
+		do {
+			gameRunning();
+		}
+		while(isGameRestart(RunningView.restartGame()));
+	}
+
+	private static boolean isGameRestart(String restartGame) {
+		return RESTART_MODE == Integer.parseInt(restartGame);
+	}
+
+	private static void gameRunning() {
+		Balls computerBalls = new Balls(NumberGenerator.autoNumberGenerator());
+
+		play(computerBalls);
+	}
+
+	private static void play(Balls computerBalls) {
+		GameResult gameResult = new GameResult();
+		while (!gameResult.isGameEnd()) {
+			gameResult = new GameResult();
+			String numbers = RunningView.insertNumber();
+			tryPlayAgain(computerBalls, numbers);
+
+			Balls userBalls = new Balls(NumberGenerator.manualNumberGenerator(numbers));
+
+			gameResult.calculateScore(computerBalls.matchThreeBalls(userBalls));
+
+			ResultView.printPitchResult(gameResult.pitchResult());
+		}
+	}
+
+	private static void tryPlayAgain(Balls computerBalls, String numbers) {
+		if(isWrongValue(numbers)){
+			RunningView.printWrongValue();
+			play(computerBalls);
+		}
+	}
+
+	private static boolean isWrongValue(String numbers) {
+		return numbers.length() != LIMIT_INSERT_NUMBER_SIZE || !numbers.matches(NUMBER_PATTERN);
+	}
 }
