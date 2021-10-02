a07441bdbf492ef98f767616bdbffa07bc25f948
kdh85 <modudamba@naver.com>
2021-10-02 11:39:37 +0900

    feat: 야구게임 결과출력 관련 뷰 생성 * 야구게임 투구에 대한 결과 출력 메소드 작성.

---
+++ src/main/java/baseball/view/ResultView.java
@@ -0,0 +1,42 @@
+package baseball.view;
+
+import java.util.Map;
+
+import baseball.model.BallType;
+
+public class ResultView {
+
+	private static final String MSG_STRIKE = "스트라이크 ";
+	private static final String MSG_BALL = "볼 ";
+	private static final String MSG_NOTHING = "낫싱 ";
+
+	public static void printPitchResult(Map<BallType, Integer> result) {
+		StringBuilder message = new StringBuilder();
+
+		for (BallType ballType : result.keySet()) {
+			addStrikeMessage(result, message, ballType);
+			addBallMessage(result, message, ballType);
+			addNothingMessage(result, message, ballType);
+		}
+
+		System.out.println(message);
+	}
+
+	private static void addNothingMessage(Map<BallType, Integer> result, StringBuilder message, BallType ballType) {
+		if (ballType.isNothing()) {
+			message.append(result.get(ballType)).append(MSG_NOTHING);
+		}
+	}
+
+	private static void addBallMessage(Map<BallType, Integer> result, StringBuilder message, BallType ballType) {
+		if (ballType.isBall()) {
+			message.append(result.get(ballType)).append(MSG_BALL);
+		}
+	}
+
+	private static void addStrikeMessage(Map<BallType, Integer> result, StringBuilder message, BallType ballType) {
+		if (ballType.isStrike()) {
+			message.append(result.get(ballType)).append(MSG_STRIKE);
+		}
+	}
+}
