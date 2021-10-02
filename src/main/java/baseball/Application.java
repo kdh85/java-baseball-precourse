e50baac653fb431f94df8175639b35a7cdb950a8
kdh85 <modudamba@naver.com>
2021-10-02 17:54:33 +0900

    refactor: 야구게임 게임 컨트롤러 추가로 인한 리팩토링 * 야구게임 진행을 담당할 컨트롤러를 추가하여 서비스를 통해서 수행 결과를 반환 받도록 구성.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import baseball.controller.BaseballController;
 import baseball.model.Game;
 import baseball.validation.BaseballMainValidation;
 import baseball.view.ResultView;
@@ -8,18 +9,20 @@ public class Application {
 
 	public static void main(String[] args) {
 
-		Game baseball;
+		Game baseballGame;
 
 		do {
-			baseball = new Game();
-			continuePlaying(baseball);
+			baseballGame = new Game();
+			continuePlaying(baseballGame);
 
-		} while (baseball.isContinueGame(BaseballMainValidation.inputRestartNumber()));
+		} while (baseballGame.isContinueGame(BaseballMainValidation.inputRestartNumber()));
 	}
 
-	private static void continuePlaying(Game baseball) {
-		while (!baseball.isGameEnd()) {
-			ResultView.printPitchResult(baseball.play(BaseballMainValidation.inputNumber()));
+	private static void continuePlaying(Game baseballGame) {
+
+		BaseballController baseballController = new BaseballController();
+		while (!baseballGame.isGameEnd()) {
+			ResultView.printPitchResult(baseballController.play(BaseballMainValidation.inputNumber(), baseballGame));
 		}
 	}
 }
