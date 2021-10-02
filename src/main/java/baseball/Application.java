2d68d4fbbfaa1627fa464a2298cf5c6d6cef70aa
kdh85 <modudamba@naver.com>
2021-10-02 17:12:44 +0900

    refactor: 야구게임 입력값 검증 전략패턴 반영에 의한 기존 클래스 리팩토링 * 기존에 메인에서 정적메소드로 사용되던 검증 코드를 검증전문 클래스로 이동. * 출력에서 담당하는 검증관련 메세지를 해당 클래스로 이동.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,57 +1,25 @@
 package baseball;
 
-import baseball.model.Balls;
-import baseball.model.GameResult;
-import baseball.util.NumberGenerator;
+import baseball.model.Game;
+import baseball.validation.BaseballMainValidation;
 import baseball.view.ResultView;
-import baseball.view.RunningView;
 
 public class Application {
 
-	private static final int RESTART_MODE = 1;
-	private static final int LIMIT_INSERT_NUMBER_SIZE = 3;
-	private static final String NUMBER_PATTERN = "^[0-9]*$";
-
 	public static void main(String[] args) {
+
+		Game baseball;
+
 		do {
-			gameRunning();
+			baseball = new Game();
+			continuePlaying(baseball);
+
+		} while (baseball.isContinueGame(BaseballMainValidation.inputRestartNumber()));
+	}
+
+	private static void continuePlaying(Game baseball) {
+		while (!baseball.isGameEnd()) {
+			ResultView.printPitchResult(baseball.play(BaseballMainValidation.inputNumber()));
 		}
-		while(isGameRestart(RunningView.restartGame()));
-	}
-
-	private static boolean isGameRestart(String restartGame) {
-		return RESTART_MODE == Integer.parseInt(restartGame);
-	}
-
-	private static void gameRunning() {
-		Balls computerBalls = new Balls(NumberGenerator.autoNumberGenerator());
-
-		play(computerBalls);
-	}
-
-	private static void play(Balls computerBalls) {
-		GameResult gameResult = new GameResult();
-		while (!gameResult.isGameEnd()) {
-			gameResult = new GameResult();
-			String numbers = RunningView.insertNumber();
-			tryPlayAgain(computerBalls, numbers);
-
-			Balls userBalls = new Balls(NumberGenerator.manualNumberGenerator(numbers));
-
-			gameResult.calculateScore(computerBalls.matchThreeBalls(userBalls));
-
-			ResultView.printPitchResult(gameResult.pitchResult());
-		}
-	}
-
-	private static void tryPlayAgain(Balls computerBalls, String numbers) {
-		if(isWrongValue(numbers)){
-			RunningView.printWrongValue();
-			play(computerBalls);
-		}
-	}
-
-	private static boolean isWrongValue(String numbers) {
-		return numbers.length() != LIMIT_INSERT_NUMBER_SIZE || !numbers.matches(NUMBER_PATTERN);
 	}
 }
--- src/main/java/baseball/view/RunningView.java
+++ src/main/java/baseball/view/RunningView.java
@@ -4,10 +4,6 @@ import nextstep.utils.Console;
 
 public class RunningView {
 
-	public static final String MSG_INSERT_NUMBER = "숫자를 입력해주세요 : ";
-	public static final String MSG_END_OF_GAME =
-		"3개의숫자를모두맞히셨습니다! 게임 끝" + System.lineSeparator() + "게임을 새로 시작하려면 1,종료하려면 2를 입력하세요." + System.lineSeparator();
-
 	private static final String MSG_ERROR_WRONG_VALUE = "[ERROR] 정상적인 범위의 숫자를 입력해 주세요.";
 
 	public static String insertNumber(String message) {
