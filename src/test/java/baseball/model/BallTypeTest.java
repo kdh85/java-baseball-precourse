179ca617ce4d0691f19c4555d68b16bc6aa9796c
kdh85 <modudamba@naver.com>
2021-10-03 18:01:28 +0900

    test: 야구게임 볼타입 메소드 테스트코드 리팩토링 * 볼 두개를 주입받아서 스트라이크, 볼, 낫싱을 반환 메소드 테스트코드 추가. * 볼 콜랙션과 볼을 주입받아서 스트라이크, 볼, 낫싱을 반환하는 메소드 추가.

--- src/test/java/baseball/model/BallTypeTest.java
+++ src/test/java/baseball/model/BallTypeTest.java
@@ -2,6 +2,8 @@ package baseball.model;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.*;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -23,5 +25,68 @@ class BallTypeTest {
 	@Test
 	void isNothing() {
 		assertThat(BallType.NOTHING.isNothing()).isTrue();
+	}
+
+	@DisplayName("같은 볼 두개를 주입하여 스트라이크가 반환된다.")
+	@Test
+	void ballDistinction_strikeTest() {
+
+		BallType ballType = BallType.ballDistinction(new Ball(1, 0), new Ball(1, 0));
+
+		assertThat(ballType).isEqualTo(BallType.STRIKE);
+	}
+
+	@DisplayName("같은 볼 두개를 주입하여 볼이 반환된다.")
+	@Test
+	void ballDistinction_ballTest() {
+
+		BallType ballType = BallType.ballDistinction(new Ball(1, 0), new Ball(1, 1));
+
+		assertThat(ballType).isEqualTo(BallType.BALL);
+	}
+
+	@DisplayName("같은 볼 두개를 주입하여 낫싱이 반환된다.")
+	@Test
+	void ballDistinction_nothingTest() {
+
+		BallType ballType = BallType.ballDistinction(new Ball(1, 0), new Ball(3, 0));
+
+		assertThat(ballType).isEqualTo(BallType.NOTHING);
+	}
+
+	@DisplayName("볼콜랙션과 볼을 받아서 볼타입 스트라이크를 반환한다.")
+	@Test
+	void ballsDistinction_StrikeTest() {
+		BallType ballType = BallType.ballsDistinction(
+			Arrays.asList(
+				new Ball(1, 0),
+				new Ball(2, 1),
+				new Ball(3, 2)
+			), new Ball(2, 1));
+		assertThat(ballType).isEqualTo(BallType.STRIKE);
+	}
+
+	@DisplayName("볼콜랙션과 볼을 받아서 볼타입 볼을 반환한다.")
+	@Test
+	void ballsDistinction_BallTest() {
+		BallType ballType = BallType.ballsDistinction(
+			Arrays.asList(
+				new Ball(1, 0),
+				new Ball(2, 1),
+				new Ball(3, 2)
+			), new Ball(2, 2));
+		assertThat(ballType).isEqualTo(BallType.BALL);
+	}
+
+	@DisplayName("볼콜랙션과 볼을 받아서 볼타입 낫싱을 반환한다.")
+	@Test
+	void ballsDistinction_nothingTest() {
+		BallType ballType = BallType.ballsDistinction(
+			Arrays.asList(
+				new Ball(1, 0),
+				new Ball(2, 1),
+				new Ball(3, 2)
+			), new Ball(4, 1));
+		assertThat(ballType).isEqualTo(BallType.NOTHING);
 	}
 }
