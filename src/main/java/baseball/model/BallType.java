4df29a6e95f35a134544194c26a3e36ff255790f
kdh85 <modudamba@naver.com>
2021-10-03 17:49:37 +0900

    refactor: 야구게임 볼타입 메소드 리팩토링 * 볼 두개를 주입받아서 스트라이크, 볼, 낫싱을 반환 메소드 추가. * 볼 콜랙션과 볼을 주입받아서 스트라이크, 볼, 낫싱을 반환하는 메소드 추가.

--- src/main/java/baseball/model/BallType.java
+++ src/main/java/baseball/model/BallType.java
@@ -1,5 +1,7 @@
 package baseball.model;
 
+import java.util.List;
+
 public enum BallType {
 	STRIKE, BALL, NOTHING;
 
@@ -11,7 +13,39 @@ public enum BallType {
 		return this.equals(BALL);
 	}
 
-	public boolean isNothing(){
+	public boolean isNothing() {
 		return this.equals(NOTHING);
+	}
+
+	public static BallType ballDistinction(final Ball computerBall, final Ball userBall) {
+
+		if (computerBall.isSameBall(userBall)) {
+			return STRIKE;
+		}
+
+		if (computerBall.isSameNumber(userBall)) {
+			return BALL;
+		}
+
+		return NOTHING;
+	}
+
+	public static BallType ballsDistinction(final List<Ball> computerBalls, final Ball userBall){
+
+		BallType type = NOTHING;
+
+		for (Ball computerBall : computerBalls) {
+			type = getStrikeOrBall(userBall, type, computerBall);
+		}
+
+		return type;
+	}
+
+	private static BallType getStrikeOrBall(Ball userBall, BallType type, Ball computerBall) {
+		BallType ballType = ballDistinction(computerBall, userBall);
+		if(!ballType.isNothing()){
+			type = ballType;
+		}
+		return type;
 	}
 }
