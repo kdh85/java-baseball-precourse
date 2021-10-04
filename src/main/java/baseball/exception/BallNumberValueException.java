c02bdb14555a53deaeed4848c78ace21b85d0b58
kdh85 <modudamba@naver.com>
2021-10-04 14:33:15 +0900

    feat: 야구게임 볼 위치 custom exception 추가 * 볼 숫자 값에 대한 명확한 에러 구분을 위한 추가 에러 객체 생성.

---
+++ src/main/java/baseball/exception/BallNumberValueException.java
@@ -0,0 +1,8 @@
+package baseball.exception;
+
+public class BallNumberValueException extends IllegalArgumentException {
+
+	public BallNumberValueException(final String message) {
+		super(message);
+	}
+}
