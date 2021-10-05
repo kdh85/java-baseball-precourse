13503f61b638633a556a9065aeca71d34bb3d1c3
kdh85 <modudamba@naver.com>
2021-10-05 10:02:45 +0900

    refactor: 야구게임 입력값 전략패턴 리팩토링 * 메인에서 각각 선언하는 시점을 변경함에 따라 기존 public 메세지에 접근할 필요 없이 메세지 출력 메소드를 호출하도록 수정함에 따라 메세지 접근제어자를 public->private로 변경 가능하게 되어 수정함.

--- src/main/java/baseball/validation/BallNumberValidationStrategy.java
+++ src/main/java/baseball/validation/BallNumberValidationStrategy.java
@@ -4,7 +4,7 @@ public class BallNumberValidationStrategy implements ValidationStrategy {
 
 	private static final int LIMIT_INSERT_NUMBER_SIZE = 3;
 	private static final String NUMBER_PATTERN = "^[1-9]*$";
-	public static final String MSG_INSERT_NUMBER = "숫자를 입력해주세요 : ";
+	private static final String MSG_INSERT_NUMBER = "숫자를 입력해주세요 : ";
 
 	@Override
 	public boolean isWrongValue(final String number) {
