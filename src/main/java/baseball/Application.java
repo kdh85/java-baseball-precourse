9350af7f5010892572049e33e858ff52bc00938d
kdh85 <modudamba@naver.com>
2021-10-05 10:03:40 +0900

    refactor: 야구게임 어플리케이션 리팩토링 * 불필요한 새로운 validation에 관련된 전략패턴를 생성을 피하기 위해 미리 생성하여 인자로 주입하는 방식으로 변경.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,14 +1,13 @@
 package baseball;
 
-import static baseball.validation.BallNumberValidationStrategy.*;
 import static baseball.validation.BaseballMainValidation.*;
-import static baseball.validation.RestartNumberValidationStrategy.*;
 import static baseball.view.RunningView.*;
 
 import baseball.controller.BaseballController;
 import baseball.model.game.Game;
 import baseball.validation.BallNumberValidationStrategy;
 import baseball.validation.RestartNumberValidationStrategy;
+import baseball.validation.ValidationStrategy;
 import baseball.view.ResultView;
 
 public class Application {
@@ -16,21 +15,26 @@ public class Application {
 	public static void main(String[] args) {
 
 		Game baseballGame;
+		ValidationStrategy ballNumberValidationStrategy = new BallNumberValidationStrategy();
+		ValidationStrategy restartNumberValidationStrategy = new RestartNumberValidationStrategy();
 
 		do {
 			baseballGame = new Game();
-			continuePlaying(baseballGame);
+			continuePlaying(baseballGame, ballNumberValidationStrategy);
 
 		} while (baseballGame.isContinueGame(
-			validationNumber(insertNumber(MSG_END_OF_GAME), new RestartNumberValidationStrategy())));
+			validationNumber(insertNumber(restartNumberValidationStrategy.messageComment()),
+				restartNumberValidationStrategy)));
 	}
 
-	private static void continuePlaying(Game baseballGame) {
+	private static void continuePlaying(Game baseballGame,
+		ValidationStrategy ballNumberValidationStrategy) {
 
 		BaseballController baseballController = new BaseballController();
 		while (!baseballGame.isGameEnd()) {
 			ResultView.printPitchResult(baseballController.play(
-				validationNumber(insertNumber(MSG_INSERT_NUMBER), new BallNumberValidationStrategy()),
+				validationNumber(insertNumber(ballNumberValidationStrategy.messageComment()),
+					ballNumberValidationStrategy),
 				baseballGame));
 		}
 	}
